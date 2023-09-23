c917c0da486e03b4a7f8abd74f55d2b477a24065
Guy Domino <guywayne.domino7@gmail.com>
2023-09-23 14:38:31 -0500

    Finished project. Attempted to add a star in the shapes list, but couldn't get the math right to allow the text to be inside of it.

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require(".")
+const { Circle, Square, Triangle, Star } = require("./lib/shapes");
 
-// Function to save the generated SVG to a file
 function saveLogo(svgLogo) {
-  fs.writeFile('logo.svg', svgLogo, (err) => {
-    if (err) throw err;
-    console.log('Generated SVG logo');
-  });
+    fs.writeFile('logo.svg', svgLogo, (err) => {
+        if (err) throw err;
+        console.log('Generated logo.svg');
+    });
 }
 
-// Function to prompt the user and generate the logo
 async function generateLogo() {
-  const shapeChoices = ['Triangle', 'Circle', 'Square'];
+    const shapeChoices = ['Triangle', 'Circle', 'Square', 'Star'];
 
-  const userInput = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters for the text:',
-      validate: (text) => text.length <= 3,
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter a color keyword or hexadecimal number for the text color:',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Select a shape:',
-      choices: shapeChoices,
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter a color keyword or hexadecimal number for the shape color:',
-    },
-  ]);
+    const userInput = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Please enter up to three characters for the text:',
+            validate: (text) => text.length <= 3,
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Please enter a color for the text color :',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Please select a shape:',
+            choices: shapeChoices,
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Please enter a color for the shape color:',
+        },
+    ]);
 
-  let shape;
+    let shape;
 
-  switch (userInput.shape) {
-    case 'Triangle':
-      shape = new Triangle();
-      break;
-    case 'Circle':
-      shape = new Circle();
-      break;
-    case 'Square':
-      shape = new Square();
-      break;
-    default:
-      console.error('Invalid shape selected.');
-      return;
-  }
+    switch (userInput.shape) {
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+        case 'Star': 
+            shape = new Star();
+            break;
+        default:
+            console.error('Invalid shape selected.');
+            return;
+    }
 
-  shape.setColor(userInput.shapeColor);
+    shape.setColor(userInput.shapeColor);
 
-  const svgLogo= `
+    const svgLogo = `
 <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
   ${shape.render()}
-  <text x="10" y="30" font-size="20" fill="${userInput.textColor}">${userInput.text}</text>
+  <text x="150" y="120" font-family="Arial" font-size="40" text-anchor="middle" fill="${userInput.textColor}">${userInput.text}</text>
 </svg>
 `;
 
-  saveLogo(svgLogo);
+    saveLogo(svgLogo);
 }
+
 generateLogo();
